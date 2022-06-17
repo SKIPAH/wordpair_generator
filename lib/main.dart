d5f83e1db991a23b6bb664ec1279cb4eb79f32f3
SKIPAH <juzzii.mehta@gmail.com>
2022-06-17 16:59:13 +0300

    lol

--- lib/main.dart
+++ lib/main.dart
@@ -8,5 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(primarySwatch: Colors.green), home: RandomWords());
+
+    //lol
   }
 }
